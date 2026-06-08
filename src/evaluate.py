bb2f47da4c3cf39282e3c0f13787edbf01f7ffb4
0yassin <yassinz8275@gmail.com>
2026-06-08 16:34:51 +0100

    optimized evaluate_position method

--- src/evaluate.py
+++ src/evaluate.py
@@ -10,6 +10,7 @@ def evaluate_position(board):
         if piece == 0:
             continue
         piece_color = 1 if piece > 0 else -1
+        pst_index = i if piece_color>0 else (i ^ 56)
         # material
         mg_score += PIECE_VALUES[abs(piece)] * piece_color
         eg_score += PIECE_VALUES[abs(piece)] * piece_color
@@ -19,61 +20,37 @@ def evaluate_position(board):
         elif abs(piece) == 5:                    phase += 4
 
         if abs(piece) == 1:
-            if piece_color>0:
-                mg_score += PST_PAWN[i] 
-                eg_score += PST_PAWN[i] * PAWN_EG_BONUS
-            else:
-                black_i = ((7 - (i // 8)) * 8) + (i % 8)
-                mg_score -= PST_PAWN[black_i]
-                eg_score -= PST_PAWN[black_i] * PAWN_EG_BONUS
+            mg_score += PST_PAWN[pst_index] * piece_color
+            eg_score += (PST_PAWN[pst_index] * PAWN_EG_BONUS) * piece_color
 
         elif abs(piece) == 2:
-            mg_score += PST_KNIGHT[i] * piece_color
-            eg_score += (PST_KNIGHT[i] * KNIGHT_EG_BONUS) * piece_color 
+            mg_score += PST_KNIGHT[pst_index] * piece_color
+            eg_score += (PST_KNIGHT[pst_index] * KNIGHT_EG_BONUS) * piece_color 
 
         elif abs(piece) == 3:
             sq = count_vision_squares(i, board, BISHOP_OFFSETS)
-            PST_score = PST_BISHOP[i]
+            PST_score = PST_BISHOP[pst_index]
             b_score = (sq * BISHOP_VISION_BONUS) + PST_score
-            if piece>0:
-                mg_score += b_score
-                eg_score += b_score
-            else:
-                mg_score -= b_score
-                eg_score -= b_score
+            mg_score += b_score * piece_color
+            eg_score += b_score * piece_color
 
         elif abs(piece) == 4:
             sq = count_vision_squares(i, board, ROOK_OFFSETS)
-            if board.turn == 'w':
-                PST_score = PST_ROOK[i]
-            else:
-                PST_score = PST_ROOK[((7 - (i // 8)) * 8) + (i % 8)]
+            PST_score = PST_ROOK[pst_index]
             b_score = (sq * ROOK_VISION_BONUS) + PST_score
-            if piece>0:
-                mg_score += b_score
-                eg_score += b_score
-            else:
-                mg_score -= b_score
-                eg_score -= b_score
+            mg_score += b_score * piece_color
+            eg_score += b_score * piece_color
 
         elif abs(piece) == 5:
             sq = count_vision_squares(i, board, QUEEN_OFFSETS)
-            PST_score = PST_QUEEN[i]
+            PST_score = PST_QUEEN[pst_index]
             b_score = (sq * QUEEN_VISION_BONUS) + PST_score
-            if piece>0:
-                mg_score += b_score
-                eg_score += b_score
-            else:
-                mg_score -= b_score
-                eg_score -= b_score
+            mg_score += b_score * piece_color
+            eg_score += b_score * piece_color
+
         elif abs(piece) == 6:
-            if piece>0:
-                mg_score += PST_KING_MG[i] 
-                eg_score += PST_KING_EG[i]
-            else:
-                black_i = ((7 - (i // 8)) * 8) + (i % 8)
-                mg_score -= PST_KING_MG[black_i] 
-                eg_score -= PST_KING_EG[black_i]
+            mg_score += PST_KING_MG[pst_index] * piece_color
+            eg_score += PST_KING_EG[pst_index] * piece_color
 
     phase = min(phase, 24)
     final_score = ((mg_score * phase) + (eg_score * (24 - phase))) // 24
